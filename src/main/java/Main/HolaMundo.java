6ee8b10c6155c0f3f816ea42357874b3f7b8a3ce
eabanqueiro <esteban.abanqueiro@udc.es>
2015-11-06 15:55:31 +0100

    Proyecto de Prueba - Hola Mundo
    Esto es un proyecto de prueba en el cual creamos un proyecto con eclipse
    utilizando maven y junit

---
+++ src/main/java/Main/HolaMundo.java
@@ -0,0 +1,18 @@
+package Main;
+
+public class HolaMundo {
+	
+	private String mensaje; 
+	
+	public HolaMundo(){
+		this.mensaje = "Hola Mundo";
+	}
+	
+	public String getMensaje() {
+		System.out.println(mensaje);
+		return this.mensaje;
+		
+	} 
+
+}
+
